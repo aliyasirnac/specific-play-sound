0efb54beca45c0941b277ceb511a0917d46ea7f2
aliyasirnc <aliyasirnac90@gmail.com>
2021-09-19 23:28:02 +0300

    added user interface (ui) & fixed some bugs.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # specific-play-sound
  kullanıcının girdiği dakikann zamanı gelince ses parçası çalması
+
+`pip install -m requirements.txt`
--- main.py
+++ main.py
@@ -1,27 +1,65 @@
-
+# -*- coding: utf-8 -*-
 from playsound import playsound
 import time,datetime
 
-def sleep_till_future(f_minute  :int ,file_name : str):
-    """
-        Bu fonksiyon şu anki zamanı alarak kullanıcının istediği bir dakikaya kadar uyur.
-    """
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+import sys
+from os import path
 
-    t = datetime.datetime.today()
-    future = datetime.datetime(t.year,t.month,t.day,t.hour,f_minute) 
+from PyQt5.uic import loadUiType
 
-    if future.minute <= t.minute:
-        print("HATA! Gelecekte geçerli bir dakika girin.")
+FORM_CLASS ,_= loadUiType(path.join(path.dirname('__file__'),"mainWindow.ui"))
 
-    else:
-
-        print(f"Şu anki zaman: {str(t.hour)}:{str(t.minute)}")
-        print(f"Kadar uyuyacak: {str(future.hour)}:{str(future.minute)}")
-
-        seconds_till_future = (future-t).seconds
-        time.sleep( seconds_till_future )
+class Main(QMainWindow, FORM_CLASS):
+    
+    
+    def __init__(self,parent = None):
+        super(Main,self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.Handel_Buttons()
         
-        print( f"{str(seconds_till_future)} saniye uyudum!")
-        playsound(file_name)
+        
+    def Handel_Buttons(self):
+        self.fileButton.clicked.connect(self.openFile)
+        self.okButton.clicked.connect(self.sleep_till_future)
+    
+    def openFile(self):
+        options = QFileDialog.Options()
+        self.fileName = QFileDialog.getOpenFileName(self,"Open Source","","Ses Dosyaları (*.mp3)", options=options)[0]
+        print(self.fileName)
+        
+    
+    def sleep_till_future(self): 
+        """
+             Bu fonksiyon şu anki zamanı alarak kullanıcının istediği bir dakikaya kadar uyur.
+        """
+        
+        
+        t = datetime.datetime.now()
+        m=int(self.spinBox.text())
+        future = datetime.datetime(t.year,t.month,t.day,t.hour,m)
+    
+        if future.minute <= t.minute:
+            print("ERROR! Enter a valid minute in the future.")
+        else:
+            print( "Current time: " + str(t.hour)+":"+str(t.minute))
+            print ("Sleep until : " + str(future.hour)+":"+str(future.minute))
+    
+            seconds_till_future = (future-t).seconds
+            time.sleep( seconds_till_future )
+            print ("I slept for "+str(seconds_till_future)+" seconds!")
+            playsound(self.fileName)
+    
+    
 
-sleep_till_future(13,"resources/atack.mp3")
+def main():
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    app.exec_()
+    
+if __name__ == "__main__":
+    main()
+
+
